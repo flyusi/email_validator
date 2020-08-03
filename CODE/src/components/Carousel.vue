071f93b221b4b01f3baa6ff52bec73c0e8ca2dc5
Blastpv <blastpv@gmail.com>
2020-08-03 18:56:38 +0500

    добавление исходного кода урака 15

---
+++ CODE/src/components/Carousel.vue
@@ -0,0 +1,58 @@
+<template>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../img/111.png" class="d-block w-100" alt="React">
+    </div>
+    <div class="carousel-item">
+      <img src="../img/112.png" class="d-block w-100" alt="Vue">
+    </div>
+        <div class="carousel-item">
+      <img src="../img/113.png" class="d-block w-100" alt="пример кода Vue">
+    </div>
+        <div class="carousel-item">
+      <img src="../img/114.png" class="d-block w-100" alt="пример кода Vue">
+    </div>
+        <div class="carousel-item">
+      <img src="../img/115.png" class="d-block w-100" alt="пример кода Angular">
+    </div>
+        <div class="carousel-item">
+      <img src="../img/116.png" class="d-block w-100" alt="Angular">
+    </div>
+        <div class="carousel-item">
+      <img src="../img/117.png" class="d-block w-100" alt="пример Node">
+    </div>
+        <div class="carousel-item">
+      <img src="../img/118.jpeg" class="d-block w-100" alt="Node">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</template>
+
+<script>
+export default {
+    name:'Carousel'
+}
+</script>
+<style scoped>
+.carousel-item {
+  height: 800px;
+  width: 100%;
+  overflow: hidden;
+}
+
+.carousel-item img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+}
+
+</style>
---
+++ CODE/src/components/Email.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="email" class="container">
+    <div class="row">
+      <!-- Форма обратной связи -->
+      <div class="col-md-4">
+        <div>
+          <h3>Обратная связь</h3>
+          <a id="Email"></a>
+          <div class="input-group mb-3">
+            <span class="input-group-text">Имя</span>
+            <input id="dname" type="text" class="form-control" />
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text">email</span>
+            <input id="demail" type="text" class="form-control" />
+            <span class="input-group-text">@example.com</span>
+          </div>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>Благодарность</option>
+            <option value="1">Ошибка</option>
+            <option value="2">Письмо директору</option>
+            <option value="3">Жалоба</option>
+          </select>
+          <p></p>
+          <div>
+            <label
+              class="form-check-label"
+              for="flexCheckIndeterminate"
+            >Согласие на обработку персональных данных</label>&nbsp;
+            <input class="form-check-input" type="checkbox" value id="flexCheckIndeterminate" />
+          </div>
+          <p></p>
+          <div class="input-group">
+            <textarea class="form-control" placeholder="Введите текст сообщения" aria-label="With textarea"></textarea>
+            <button @click="sendform" class="btn btn-info">Отправить</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Email",
+
+  methods: {
+    // валидация формы
+    sendform() {
+      var dn = document.getElementById("dname");
+      var em = document.getElementById("demail");
+      var ch = document.getElementById("flexCheckIndeterminate");
+
+      if (dn.value == "") {
+        alert("Пожалуйста, введите Ваше имя");
+        return false;
+      } else if (em.value == "") {
+        alert("Пожалуйста, введите электронный адрес");
+        return false;
+      } else if (ch.checked == false) {
+        alert("Предоставьте согласие на обработку персональных данных");
+        return false;
+      }
+      alert("Письмо отправлено!!!");
+      return true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.col-md-4 {
+  text-align: left;
+  margin: 50px 0 0 0;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #2c3e50;
+  text-indent: 25px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 30em;
+}
+</style>
