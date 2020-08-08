558c088936b4cd17929b2abe2db6697dcea495d3
flyusi <flyusi111@gmail.com>
2020-08-08 16:45:17 +0500

    main logic for validation

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-//Валидация почты
-function checkEmail(emailId) {
-    // let email = $(this).val();
-  
-    // if (email.length > 0
-    // && (email.match(/.+?\@.+/g) || []).length !== 1) {
-    //     console.log('invalid');
-    //     alert('Вы ввели некорректный e-mail!');
-    // } else {
-    //     console.log('valid');
-    //     alert('Вы ввели корректный e-mail!');
-    // }
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailId)){
-    alert("Вы ввели верный адрес");
-    return true;
-    }    
-    return false;
+/**
+ * ПЛАН:
+ * 1. проверка синтаксиса с помощью регулярных выражений
+ * 2. проверка доступности почтового сервера
+ * 3. проверка самого ящика по SMTP (?)
+ * 
+ */
+
+
+function checkEmail(email) {
+    // Проверка синтаксиса согласно  http://tools.ietf.org/html/rfc3696#page-5
+    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (email.match(mailFormat)){
+        alert("Формат адреса верный"); // можно будет убрать данное сообщение
+        return true;
     }
-    function ValidateEmail(){
-        var emailID=document.form.email;
-        if ((emailID.value==null)||(emailID.value=="")){
-            alert("Пожалуйста, введите адрес")
-            emailID.focus()
-            return false
-        }
-        if (checkEmail(emailID.value)==false){
+    return false;
+}
+    
+
+function checkMailServer(){
+    // проверка почтового сервера DNS-запросом
+
+}
+
+function validateEmail(){
+    var emailID = document.form.email;
+    
+    if (checkEmail(emailID.value) === true){
+        // тогда проверяем доступность сервера
+        if(checkMailServer() === true){
+            // тогда проверяем наличие ящика
+            if(checkRecipient() === true){
+                alert("Указанный почтовый ящик существует");
+            }else{
+                alert("Указанного получателя нет на данном почтовом сервере")
+            }
+        }else{
             emailID.value=""
-            alert("Введенный адрес неверный");
+            alert("Указанный почтовый сервер недоступен");
             emailID.focus()
-            return false
         }
-            // alert('valid');
-            // return true
-     }
+    }else{
+        // если синтаксис неверный - в дальнейших проверках нет смысла
+        emailID.value=""
+        alert("Формат адреса неверный");
+        emailID.focus()
+        // return false
+    }
+}
